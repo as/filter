76bd931697bc4b3911680355d882df0294140aee
as <as.utf8@gmail.com>
2019-04-13 18:05:12 -0700

    add unique filter

---
+++ uniq/filter.go
@@ -0,0 +1,51 @@
+// Package uniq implements a uniqueness filter. It supports atomic
+// multi-key put and delete operations. There is currently no lookup
+// operation (deemed unnecessary for its current purpose).
+//
+// TODO(as): Replace sync mutexes with downgradable locks
+// and benchmark
+package uniq
+
+import "sync"
+
+// New returns a ready-to-use deterministic uniqueness filter
+func New() *Filter {
+	return &Filter{m: map[string]struct{}{}}
+}
+
+// Unique is a non-counting deterministic existence
+// filter. It supports atomic multi-key put and delete
+// operations.
+type Filter struct {
+	sync.RWMutex
+	m map[string]struct{}
+	// TODO(as): use sys package
+	_ [96]byte // cache line pad
+}
+
+// PutAll either commits the entire list of keys to the
+// filter, or none at all. It returns false if any of the
+// provided keys exist in the filter.
+func (f *Filter) PutAll(key ...string) bool {
+	f.Lock()
+	defer f.Unlock()
+	for _, key := range key {
+		if _, exist := f.m[key]; exist {
+			return false
+		}
+	}
+	for _, key := range key {
+		f.m[key] = struct{}{}
+	}
+	return true
+}
+
+// DelAll removes the list of keys from the filter. It does
+// not check for existence.
+func (f *Filter) DelAll(key ...string) {
+	f.Lock()
+	defer f.Unlock()
+	for _, key := range key {
+		delete(f.m, key)
+	}
+}
---
+++ uniq/filter_test.go
@@ -0,0 +1,73 @@
+package uniq
+
+import "testing"
+
+func TestFilter(t *testing.T) {
+	f := New()
+	if !f.PutAll("a", "b", "c") {
+		t.Fatal("failed to put fresh value list")
+	}
+	if f.PutAll("a", "b", "c") {
+		t.Fatal("duplicate insertion of a,b,c")
+	}
+	if f.PutAll("a") {
+		t.Fatal("duplicate partial insertion of a")
+	}
+	if !f.PutAll("d") {
+		t.Fatal("failed to put fresh values after successful insertion of unrelated values")
+	}
+	f.DelAll("d")
+	if !f.PutAll("d") {
+		t.Fatal("failed to delete 'd'")
+	}
+	f.DelAll("a", "b", "c")
+	if !f.PutAll("a", "b", "c") {
+		t.Fatal("failed to delete a,b,c")
+	}
+}
+
+func testPutDel(f *Filter, put, del []string, done chan bool) {
+	for {
+		select {
+		case <-done:
+		default:
+		}
+		f.PutAll(put...)
+		f.DelAll(del...)
+	}
+}
+
+func TestFilterConcurrent(t *testing.T) {
+	const N = 1000000
+	f := New()
+	c := make(chan bool)
+	defer close(c)
+	go testPutDel(f, []string{"a", "b"}, []string{"c", "d", "e"}, c)
+	go testPutDel(f, []string{"c", "d", "e"}, []string{"a", "b"}, c)
+	for i := 0; i < N; i++ {
+		if !f.PutAll("z") {
+			t.Fatal("failed to put z")
+		}
+		f.DelAll("z")
+	}
+}
+
+func BenchmarkFilter(b *testing.B) {
+	f := New()
+	for n := 0; n < b.N; n++ {
+		f.PutAll("z")
+		f.DelAll("z")
+	}
+}
+
+func BenchmarkFilterConcurrentLoadx2(b *testing.B) {
+	f := New()
+	c := make(chan bool)
+	defer close(c)
+	go testPutDel(f, []string{"a", "b"}, []string{"c", "d", "e"}, c)
+	go testPutDel(f, []string{"c", "d", "e"}, []string{"a", "b"}, c)
+	for n := 0; n < b.N; n++ {
+		f.PutAll("z")
+		f.DelAll("z")
+	}
+}
